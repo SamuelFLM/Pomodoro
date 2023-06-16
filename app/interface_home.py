7d741224ddf42b034f6656660bcd664d302cc0da
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-06-16 12:52:25 -0300

    corrigindo testes

--- app/interface_home.py
+++ app/interface_home.py
@@ -29,9 +29,8 @@ class Home:
         rodape = [
              sg.Image(filename="dependency//img//iniciar sessao (1).png", background_color=self.background_color,
                       pad=(0, (45, 0)), enable_events=True, key="iniciar_sessao_de_foco")]
-        test = [sg.Button("test")]
 
-        layout = [head_imagem, main, rodape, test]
+        layout = [head_imagem, main, rodape]
         window = sg.Window("Sessão de foco", layout=layout, size=(272, 442), margins=(0, 0), grab_anywhere=True,
                            element_justification='c', icon="dependency//img//ico.ico")
         return  window
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,14 +1,33 @@
 import unittest
-from app.interface_home import Home
+from unittest.mock import patch, MagicMock
+import PySimpleGUI as sg
+from app.interface_home import Home  # Substitua "sua_classe" pelo nome do módulo ou arquivo que contém a classe
 
-class HomeTest(unittest.TestCase):
+
+class TestSuaClasse(unittest.TestCase):
 
     def setUp(self) -> None:
-       pass
+        self.home = Home()
 
+    def test_init(self):
+        self.assertEqual(self.home.background_color, sg.theme_background_color("#C6E59D"))
 
-    def test_back_1(self):
-        pass
+    def test_front(self):
+        with patch('PySimpleGUI.Window') as mock_window:
+            home = Home()
+            home.front()
+            mock_window.assert_called_with("Sessão de foco", layout=unittest.mock.ANY, size=(272, 442), margins=(0, 0),
+                                           grab_anywhere=True,
+                                           element_justification='c', icon="dependency//img//ico.ico")
+
+    def test_back(self):
+        with patch('builtins.print') as mock_print:
+            home = Home()
+            window = MagicMock()
+            window.read = MagicMock(return_value=(sg.WIN_CLOSED, "1"))
+            home.back(window)
+            mock_print.assert_not_called()
+
 
 if __name__ == '__main__':
     unittest.main()
