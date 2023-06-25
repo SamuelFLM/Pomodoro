40bb98e3c7261cad6b691941c8418734352c1c04
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-06-25 18:49:29 -0300

    corrigindo testes

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,12 @@
+import PySimpleGUI as sg
 
-class Utils:
+layout = [[sg.Button('Button with Image', image_filename='dependency/img/chevron-up-24.png', expand_x=True)]]
+window = sg.Window('Window with Image Button', layout, size=(200,300))
 
-    def __init__(self):
-        pass
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+
+window.close()
+
